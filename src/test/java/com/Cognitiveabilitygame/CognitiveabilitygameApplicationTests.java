4cb06cb95d4c966d0901c542c649511ed65968f6
Manish kumar Shah <142222577+Manish42-U@users.noreply.github.com>
2025-09-30 19:15:00 +0530

    Update CognitiveabilitygameApplicationTests.java

--- src/test/java/com/Cognitiveabilitygame/CognitiveabilitygameApplicationTests.java
+++ src/test/java/com/Cognitiveabilitygame/CognitiveabilitygameApplicationTests.java
@@ -3,7 +3,8 @@ package com.Cognitiveabilitygame;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
+@SpringBootTest(classes = CognitiveabilitygameApplication.class)
 class CognitiveabilitygameApplicationTests {
 
 	@Test
